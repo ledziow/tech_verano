07d57b7c0900d875ee37639517710b87b954843a
ledziow <ledziow@users.noreply.github.com>
2024-03-02 16:45:48 +0100

    Add files via upload

--- verano.py
+++ verano.py
@@ -99,7 +99,7 @@ class TECH_VERANO:
         _LOGGER.debug("Updating cookies for Tech API ...")
         for k in response.raw_headers:
             if "Set-Cookie" in k[0].decode():
-                if (set_session := k[1].decode().replace("session=","")) is  not None:
+                if (set_session := k[1].decode("utf-8").replace("session=","")) is  not None:
                     self.session.cookie_jar.update_cookies({"session": set_session})
                     _LOGGER.debug("Cookies for Tech API were updated!")
                     break
